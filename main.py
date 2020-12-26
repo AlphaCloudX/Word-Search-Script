d71b72b9bde88e801eb3288a1be7697abc9f5c8b
AlphaCloudX <66267343+AlphaCloudX@users.noreply.github.com>
2020-12-26 14:35:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,3 +34,50 @@ finalList = []
 
 array = arrayy
 wordToFind = wordToFindd
+
+# Basic Methods
+for waysToFind in range(4):
+
+    # Row
+    if waysToFind == 0: row = True; board = array; reversee = False; print("Searching Row")
+
+    # Reverse Row
+    if waysToFind == 1: row = True; board = array; reversee = True; print("Searching Reverse Row")
+
+    # Column
+    if waysToFind == 2: row = False; board = column(array); reversee = False; print("Searching Column")
+
+    # Reverse Column
+    if waysToFind == 3: row = False; board = column(array); reversee = True; print("Searching Reverse Column")
+
+    for words in range(len(wordToFind)):
+
+        for lengthOfBoard in range(len(board)):
+
+            for i in range(len(board[lengthOfBoard])):
+
+                wordBeingSearched = []
+                t = []
+
+                wordBeingSearched.append(board[lengthOfBoard][i:len(wordToFind[words]) + i])
+
+                if reversee:
+                    wordBeingSearched == reverse(wordBeingSearched)
+
+                if len(wordBeingSearched[0]) == len(wordToFind[words]):
+
+                    print("Checking Combination: " + str(wordBeingSearched))
+
+                    if wordBeingSearched[0] == wordToFind[words]:
+
+                        for x in range(0, len(wordToFind[words])):
+                            t.append(i+x+1)
+
+                        print(str(wordToFind[words]) + str(" is found at " + "Row: " if row else "Column: ") + str(lengthOfBoard) + " | Letter Positions are: " + str(t))
+
+                        finalList.append([wordToFind[words], str("Row: " if row else "Column: ") + str(int(lengthOfBoard) +1), str("Column: " if row else "Row: ") + str(t)])
+                else:
+                    pass
+
+
+print(finalList)
